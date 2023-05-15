b164ab5f76e5c9db1f43355eee97293671db7b7b
antoniorebelo <tz.ajsr@gmail.com>
2023-05-15 15:21:18 +0100

    Config and test Sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Student from '../models/Student';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const newStudent = await Student.create({
+      name: 'Maria',
+      lastname: 'Rebelo',
+      email: 'maria.ajsr@gmail.com',
+      age: 6,
+      weight: 18,
+      height: 1.05,
     });
+    res.json(newStudent);
   }
 }
 
