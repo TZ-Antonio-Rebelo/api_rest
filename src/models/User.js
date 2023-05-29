5693d4dfa7ca2319ec1d3489e8d52a04f2c1bbf1
antoniorebelo <tz.ajsr@gmail.com>
2023-05-29 18:13:42 +0100

    CRUD

--- src/models/User.js
+++ src/models/User.js
@@ -24,16 +24,6 @@ export default class User extends Model {
           isEmail: {
             msg: 'Invalid email.',
           },
-          isUnique(value, next) {
-            User.findOne({ where: { email: value } })
-              .then((user) => {
-                if (user) {
-                  return next('E-mail já existe, tente outro!');
-                }
-                return next();
-              })
-              .catch((err) => next(err));
-          },
         },
       },
       password_hash: {
@@ -55,7 +45,9 @@ export default class User extends Model {
     });
 
     this.addHook('beforeSave', async (user) => {
-      user.password_hash = await bcryptjs.hash(user.password, 8);
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
     });
 
     return this;
