fa8c02986004fb0a253f82be7ab3b1f2cd708f28
antoniorebelo <tz.ajsr@gmail.com>
2023-06-07 15:43:06 +0100

    Students Routes

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Student from '../models/Student';
-
 class HomeController {
   async index(req, res) {
-    const newStudent = await Student.create({
-      name: 'Maria',
-      lastname: 'Rebelo',
-      email: 'maria.ajsr@gmail.com',
-      age: 6,
-      weight: 18,
-      height: 1.05,
-    });
-    res.json(newStudent);
+    res.json('Index');
   }
 }
 
---
+++ src/database/migrations/20230607142728-change-email-student-unique.js
@@ -0,0 +1,12 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.changeColumn(
+    'students',
+    'email',
+    {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  ),
+  down: () => {},
+};
